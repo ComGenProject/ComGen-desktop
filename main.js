f34b50c61d3026aa314428cf79f904211c058892
Esteban Basson <98833964+stbn1699@users.noreply.github.com>
2023-03-07 13:14:11 +0100

    init commit

---
+++ main.js
@@ -0,0 +1,32 @@
+//import electron
+const { app, BrowserWindow } = require('electron');
+
+//create window
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        autoHideMenuBar: true,//hide the menuBar
+    });
+
+    win.loadFile('index.html');
+};
+
+//display window when ready
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+
+});
+
+//close app when all windows are closed
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
